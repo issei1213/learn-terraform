8c798256b29a134f498f7f614b146931ae0a8095
issei1213 <59830008+issei1213@users.noreply.github.com>
2025-10-20 16:01:33 +0900

    Route53のホストゾーンとALB用DNSレコード、domain_name出力を追加

---
+++ practice-terraform/route53.tf
@@ -0,0 +1,31 @@
+# ホソトゾーンのデータソース定義
+locals {
+  name = "issei1213.com"
+}
+
+# ホストゾーンのリソース定義
+resource "aws_route53_zone" "test_example" {
+  name          = "issei1213.com"
+  force_destroy = false
+
+  tags = {
+    Name = "issei1213.com"
+  }
+}
+
+# ALBのDNSレコードの定義
+resource "aws_route53_record" "example" {
+  zone_id = aws_route53_zone.test_example.zone_id
+  name    = local.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.example.dns_name
+    zone_id                = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "domain_name" {
+  value = "http://${aws_route53_record.example.name}"
+}
