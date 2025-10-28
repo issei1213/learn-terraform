9b10a2a1b7b78b34d1e2d3425271051dc46f5169
issei1213 <59830008+issei1213@users.noreply.github.com>
2025-10-28 14:39:02 +0900

    practice-terraform に SSM パラメータを追加およびバッチコンテナ定義を更新：/db/username と /db/password を aws_ssm_parameter で作成（password は SecureString、lifecycle で value を ignore_changes に設定）、batch_container_definitions.json に secrets を追加してコンテナに渡すようにし、コマンドを /usr/bin/env に変更

---
+++ practice-terraform/store_parameter.tf
@@ -0,0 +1,20 @@
+# データベースのユーザー名の定義
+resource "aws_ssm_parameter" "db_username" {
+  name        = "/db/username"
+  value       = "root"
+  type        = "String"
+  description = "データベースのユーザー名"
+}
+
+
+# データベースのパスワードの定義
+resource "aws_ssm_parameter" "db_password" {
+  name        = "/db/password"
+  value       = "uninitialized"
+  type        = "SecureString"
+  description = "データベースのパスワード"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
